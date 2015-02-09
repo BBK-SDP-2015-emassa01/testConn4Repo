4901e2b0490821a7f796d339ee28d46d00b30903
LiliyaStefanova <liliya.stefanova@gmail.com>
2015-02-09 21:24:05 +0000

    test commit: added a field with a getter

--- src/testie/HelloWorldie.java
+++ src/testie/HelloWorldie.java
@@ -1,9 +1,18 @@
 package testie;
 
 public class HelloWorldie {
-	
+
+	private String sentence = "I said";
+
+	public String getSentence(){
+		return this.sentence;
+	}
+
+
 	public static void main(String[] args){
-		System.out.println("Hello Liliya!" );
+
+		HelloWorldie hw = new HelloWorldie();
+		System.out.println(hw.getSentence() + " Hello Liliya!" );
 	}
 
 }
