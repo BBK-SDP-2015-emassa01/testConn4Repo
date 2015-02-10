2f2ca248eb2c33b88aabdcad8692ac0ee662c77a
Esha <ubemas001@mail.bbk.ac.uk>
2015-02-10 12:51:06 +0000

    Update HelloWorldie.java

--- src/testie/HelloWorldie.java
+++ src/testie/HelloWorldie.java
@@ -5,6 +5,7 @@ public class HelloWorldie {
 	public static void main(String[] args){
 		System.out.println("Hello Liliya! I wanted to test if I could revert your change, and I did." );
 		System.out.println("I'm now going to see if I can write here, and pull the change to my IDE." );
+		System.out.println("Wow all I had to do was press sync in my GitHub application!" );
 	}
 
 }
