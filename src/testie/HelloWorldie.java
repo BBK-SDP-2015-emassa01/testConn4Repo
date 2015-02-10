30fdfa73666657854e13714d325e5a3e045715dd
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-02-10 12:42:11 +0000

    more changes to lilies mods

--- src/testie/HelloWorldie.java
+++ src/testie/HelloWorldie.java
@@ -1,9 +1,35 @@
 package testie;
 
 public class HelloWorldie {
-	
+
+	private String sentence = "I said";
+
+	public String getSentence(){
+		return this.sentence;
+	}
+
+
 	public static void main(String[] args){
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 		System.out.println("Hello Liliya!" );
+		
+=======
+
+		HelloWorldie hw = new HelloWorldie();
+		System.out.println(hw.getSentence() + " Hello Liliya!" );
+>>>>>>> FETCH_HEAD
+=======
+
+		HelloWorldie hw = new HelloWorldie();
+		System.out.println(hw.getSentence() + " Hello Liliya!" );
+>>>>>>> FETCH_HEAD
+=======
+
+		HelloWorldie hw = new HelloWorldie();
+		System.out.println(hw.getSentence() + " Hello Liliya!" );
+>>>>>>> FETCH_HEAD
 	}
 
 }
---
+++ src/testie/test.java
@@ -0,0 +1,5 @@
+package testie;
+
+public class test {
+
+}
